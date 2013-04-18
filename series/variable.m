4e2f8ab47f4dffe58c44949a1f453ee69f46c1b0
Chino <joseignaciosg9@gmail.com>
2013-04-17 23:53:23 -0300

    cambios para que ande en matlab

--- series/variable.m
+++ series/variable.m
@@ -4,7 +4,7 @@
 %etta factor de aprendizaje
 %S salidas esperadas 
 
-function V,D,A,s,o = variable(E,A,P,s,eta)
+function [V,D,A,s,o] = variable(E,A,P,s,eta)
 
 m=max(P);
 V=zeros(length(P)-1, m + 1);
@@ -13,8 +13,8 @@ V(:,1) = -1; %el primer elemento de cada fila es -1 porque corresponde al umbral
 i = 1;
 while(i <= P(2))
 	V(1,i+1) = tanh(A(i,1:P(1)+1,1) * E');
-	i++;
-endwhile
+	i=i+1;
+end
 
 %se calculan las salidas de las demas capas usando como entrada las salida de las anteriores
 i = 2;
@@ -22,10 +22,10 @@ while(i < length(P))
 	j = 1;
 	while(j <= P(i+1))
 		V(i,j+1) = tanh(A(j,1:P(i)+1,i) * V(i-1,1:P(i)+1)');
-		j++;
-	endwhile
-	i++;
-endwhile
+		j=j+1;
+    end
+	i=i+1;
+end
 
 %se calcula la salida final
 o = V(length(P)-1,2);
@@ -39,24 +39,24 @@ while (i < length(P))
 	while( k <= P(length(P) - i + 1) )
 		j = 1;% valores en V; ds en la capa anterior
 		while (j <= P(length(P) - i + 2) )
-			D(i,k) += D(i-1,j) * A(P(length(P) - i + 2),k+1,i);
-			j++;
-		endwhile
-		k++;
-	endwhile
-	i++;
-endwhile
+			D(i,k) = D(i,k)+ D(i-1,j) * A(P(length(P) - i + 2),k+1,i);
+			j=j+1;
+        end
+		k=k+1;
+    end
+	i=i+1;
+end
 
 %actualizo los pesos de la primera matriz porque uso E en vez de V
 j = 1;
 while(j <= P(2)) %cantidad de neuronas en la capa siguiente. determina la cantidad de filas en mi matriz.
 	k = 1;
 	while(k <= P(1) + 1) %cantidad de neuronas en mi capa + 1. determina la cantidad de columnas en mi matriz.
-		A(j, k, 1) += eta * (1-(tanh( A(j,1:P(1)+1,1) * E' ))**2) * D(length(P)-1,j) * E(k);
-		k++;
-	endwhile
-j++;
-endwhile
+		A(j, k, 1) = A(j, k, 1) + eta * (1-(tanh( A(j,1:P(1)+1,1) * E' ))^2) * D(length(P)-1,j) * E(k);
+		k=k+1;
+    end
+j=j+1;
+end
 
 %actualizo las matrices de pesos
 i = 2;
@@ -65,24 +65,24 @@ while(i < length(P)) %cantidad de matrices
 	while(j <= P(i + 1)) %cantidad de neuronas en la capa siguiente. determina la cantidad de filas en mi matriz.
 		k = 1;
 		while(k <= P(i) + 1) %cantidad de neuronas en mi capa + 1. determina la cantidad de columnas en mi matriz.
-			A(j, k, i) += eta * (1-(tanh( A(j,1:P(i)+1,i) * V(i-1,1:P(i)+1)' ))**2) * D(length(P)-i,j) * V(i-1,k);
-			k++;
-		endwhile
-	j++;
-	endwhile
-i++;
-endwhile
+			A(j, k, i) =A(j, k, i) + eta * (1-(tanh( A(j,1:P(i)+1,i) * V(i-1,1:P(i)+1)' ))^2) * D(length(P)-i,j) * V(i-1,k);
+			k=k+1;
+        end
+	j=j+1;
+    end
+i=i+1;
+end
 
 ret = 0;
 if (s == 1)
 	if ( o >= 0.5)
 		ret = 1;
-	endif
+    end
 else
 	if ( o < 0.5)
                 ret = 1;
-        endif
-endif
+    end
+end
 
 
-endfunction
+end
--- series/variablewrapper.m
+++ series/variablewrapper.m
@@ -1,7 +1,8 @@
 %entrena a una red iterativamente
 %para xor con unidad no lineal tangente hiperbÃ³lica
+%correr así
 
-function V,D,A,s,o,count,dif = variablewrapper(P,eta,err)
+function [V,D,A,s,o,count,dif] = variablewrapper(P,eta,err)
 
 patterns = [-1 0 0;
 -1 0 1;
@@ -25,24 +26,33 @@ A(:,:,2)=[0.67 0.23 0.41; 0 0 0];
 dif = 10;
 old = 11;
 threshold = 50000;
+errors = [];
+x = [];
 while(dif > err && threshold > 0 && abs(dif-old) > 1e-6)
 	i=1;
 	old = dif;
 	dif = 0;
-	while(i<=rows(patterns))
+	while(i<=size(patterns,2))
 		[V,D,A,s,o] = variable(patterns(i,:),A,P,p(i),eta);
-		i++;
+		i=i+1;
 		dif = dif + (s-o)^2;
-	endwhile
+    end
 	dif = dif / 4; % four patterns;
-	count ++;
-	threshold--;
-endwhile
+    dif
+    errors = [dif errors];
+    x = [count x];
+	count= count+1;
+	threshold= threshold-1;
+end
+
 
 if(threshold <= 0 || (old - dif)<1e-6)
-	disp("Solution not reached");
-	disp("old");disp(old);
-	disp("dif");disp(dif);
-endif
+	disp('Solution not reached');
+	disp('old');disp(old);
+	disp('dif');disp(dif);
+end
 
-endfunction
+
+plot(x,errors);
+
+end
