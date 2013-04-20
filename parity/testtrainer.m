5f00483b50ff51355cbf2ac31eba35292722d48a
nloreti <nloreti@gmail.com>
2013-04-20 16:57:53 -0300

    test variable

---
+++ parity/testtrainer.m
@@ -0,0 +1,42 @@
+%pueba si la red neuronal aprendió a determinar la paridad de 2 a 5 
+%entradas
+
+function [count] = testtrainer(series,A,P)
+
+
+%maximo valor de P para formar la matriz
+m = max(P);
+
+windowsize = P(1);
+
+index = P(1) -1; %resto -1 para que de bien el index en el vector testing
+
+%Series a tomar en cuenta para entrenamiento
+series = series./3.8;
+
+%Variables
+x = [];
+os=[];
+ss=[];
+count = 0;
+
+while(count < 1)
+	i=1;
+	while(i<=(length(series)-windowsize))
+		s = series(i+windowsize);
+		[s,o] = variable4testing(series(i:i+windowsize-1),A,P,s);
+		i=i+1;
+		final_s = s * 3.8;
+		final_o = o * 3.8;
+		os = [os final_s];
+    	ss = [ss final_o];
+    	x = [i x];
+    end
+	count = count+1;
+end
+
+figure(1);
+plot(x,ss,x,os);
+
+end
+
