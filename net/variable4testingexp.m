a24d216e060b01d362a5caa6bd5da09fdfb7c764
nloreti <nloreti@gmail.com>
2013-04-24 12:43:24 -0300

    asd

--- net/variable4testingexp.m
+++ net/variable4testingexp.m
@@ -4,6 +4,7 @@ function [s,o] = variable4testingexp(E,A,P,s,beta)
     max_neurons =max(P);
     m = length(P); %layers number
     V = zeros(m, max_neurons + 1); %+1 for the threshold
+
     
     %the first row of V are the inputs
     aux = zeros(1,length(V(1,:))-length(E)-1);
@@ -15,6 +16,7 @@ function [s,o] = variable4testingexp(E,A,P,s,beta)
         V(1,:) = E;
     end
   
+  
     %computes outputs 
     i = 1;
     while(i<m)
@@ -22,7 +24,7 @@ function [s,o] = variable4testingexp(E,A,P,s,beta)
        V(i+1,:) =  [-1 (1./(1+exp(membrane_potential' .* (-2*beta))))];
        i=i+1;
     end
-       
+
     %final output
     o = V(m,2);
 
